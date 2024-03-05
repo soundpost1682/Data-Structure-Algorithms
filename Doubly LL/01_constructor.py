6566bc713753f103d7ab5ff09bcbcb3b360e0ae3
evan <guarneri1682@gmail.com>
2024-03-05 15:39:25 -0800

    240305_DLL_cnstrct

---
+++ Doubly LL/01_constructor.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+my_DLL = DoublyLinkedList(7)
+my_DLL.print_list()
