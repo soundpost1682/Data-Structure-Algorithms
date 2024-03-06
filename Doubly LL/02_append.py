c953f7570969d30ae048ddee56922e0dd7ecf6ce
evan <guarneri1682@gmail.com>
2024-03-05 16:39:27 -0800

    240305_DLL_pop

---
+++ Doubly LL/02_append.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)       # initialize a new node 
+        if self.head is None:        
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+my_DLL = DoublyLinkedList(7)
+my_DLL.append(2)
+my_DLL.print_list()
---
+++ Doubly LL/03_pop.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)       # initialize a new node 
+        if self.head is None:        
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None       # disconnect the node 
+            temp.prev = None          # disconnect the node
+        self.length -= 1
+        return temp
+    
+my_DLL = DoublyLinkedList(7)
+my_DLL.append(2)
+
+print(my_DLL.pop())
+print(my_DLL.pop())
+print(my_DLL.pop())
+
+
