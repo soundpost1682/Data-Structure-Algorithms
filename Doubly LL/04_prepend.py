a568f402463f3061477cccae64003d34bc338ec6
evan <guarneri1682@gmail.com>
2024-03-13 00:36:12 -0700

    240313_DLL_prepend

---
+++ Doubly LL/04_prepend.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)       # initialize a new node 
+        if self.head is None:        
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None       # disconnect the node 
+            temp.prev = None          # disconnect the node
+        self.length -= 1
+        return temp
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    
+my_DLL = DoublyLinkedList(32)
+my_DLL.append(24)
+
+my_DLL.prepend(1)
+
+my_DLL.print_list()
+
+
+
