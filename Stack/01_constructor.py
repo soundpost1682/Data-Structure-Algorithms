3198de3278689d6fd42ec2dd3b88017ecf6c0a0f
evan <guarneri1682@gmail.com>
2024-04-17 00:33:40 -0700

    Stack Push

--- Stack/01_constructor.py
+++ Stack/01_constructor.py
@@ -15,6 +15,15 @@ class Stack:
             print(tmp.value)
             tmp = tmp.next
 
-my_stack = Stack(4)
+    def push(self,value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
 
+my_stack = Stack(32)
+my_stack.push(25)
 my_stack.print_stack()
