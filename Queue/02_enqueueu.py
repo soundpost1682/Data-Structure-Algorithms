1822d6c2c573891b7c836cb07e3aa943a9c09a25
evan <guarneri1682@gmail.com>
2024-04-17 23:47:56 -0700

    Queue enqueue

---
+++ Queue/02_enqueueu.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        tmp = self.first
+        while tmp is not None:
+            print(tmp.value)
+            tmp = tmp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+
+my_queue = Queue(11)
+my_queue.print_queue()
+my_queue.enqueue(22)
+
+my_queue.print_queue()
