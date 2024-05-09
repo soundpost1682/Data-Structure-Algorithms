c09f7d8440d7129399248659a81bdc56e1193580
evan <guarneri1682@gmail.com>
2024-05-08 17:50:47 -0700

    BST contains

--- Binary Search Trees/03_BST_contains.py
+++ Binary Search Trees/03_BST_contains.py
@@ -27,13 +27,28 @@ class BinarySearchTree:
                     tmp.right = new_node
                     return True
                 tmp = tmp.right
+    
+    def contains(self, value):
+        tmp = self.root
+        while tmp is not None:
+            if value < tmp.value:
+                tmp = tmp.left
+            elif value > tmp.value:
+                tmp = tmp.right
+            else: return True
+        return False
 
 my_tree = BinarySearchTree()
+my_tree.insert(47)
 my_tree.insert(21)
-my_tree.insert(14)
-my_tree.insert(35)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+
+print(my_tree.contains(27))
+print(my_tree.contains(17))
+
 
